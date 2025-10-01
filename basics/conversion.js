9b8e71f166b48acdabacd089b6558dd96e8f0482
an-shull <118686764+an-shull@users.noreply.github.com>
2025-10-01 13:53:28 +0000

    conversions done

---
+++ basics/conversion.js
@@ -0,0 +1,16 @@
+let score = "69abc"
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"69" => 69
+// "69" => NaN
+// true => 1, false => 0
+
+let isLoggedIn = 1
+
+let bool = Boolean(isLoggedIn)
+console.log(bool)

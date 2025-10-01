c7c7b44c780ae4cea841d3f38c09d3dbf06fe4cd
an-shull <118686764+an-shull@users.noreply.github.com>
2025-10-01 13:38:20 +0000

    datatypes done

---
+++ basics/datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js node as newer js
+
+console.log(3+3) 
+console.log("hello")
+
+// code readability should be high 
+
+let name = "anshul"
+let age = 21
+let isLoggedIn = true
+
+// number  => 2 ^ 53
+//bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => unassigned 
+// symbol => unique
+
+// object 
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof isLoggedIn)
+console.log(typeof null)
+console.log(typeof undefined)

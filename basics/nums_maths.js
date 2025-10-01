f1a9b27682bffdff6ddd1a612c58d0238aaf13fb
an-shull <118686764+an-shull@users.noreply.github.com>
2025-10-01 17:49:26 +0000

    nums done

---
+++ basics/nums_maths.js
@@ -0,0 +1,39 @@
+// nums=>
+
+const score = 6969
+console.log(score)
+
+const balance = new Number(69)
+console.log(balance)
+
+console.log(balance.toString())
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+
+const num = 69.699
+console.log(num.toPrecision(2))
+
+const hund = 1000000
+console.log(hund.toLocaleString())
+console.log(hund.toLocaleString('en-IN'))
+
+// maths =>
+
+console.log(Math)
+console.log(Math.abs(-4))
+
+console.log(Math.round(68.89))
+console.log(Math.floor(68.89))
+console.log(Math.ceil(68.89))
+
+console.log(Math.min(2,4,6,8))
+console.log(Math.max(2,4,6,8))
+
+console.log(Math.random())
+console.log(Math.random()*10)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min +1) + min))
+// formula to get  random values between two nums

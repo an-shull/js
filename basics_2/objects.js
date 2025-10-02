e45732173bfb03869ac6c830cf54b7f7e12d2ae0
an-shull <118686764+an-shull@users.noreply.github.com>
2025-10-02 08:36:35 +0000

    obj 1

---
+++ basics_2/objects.js
@@ -0,0 +1,42 @@
+// singleton 
+
+// object literals
+
+const mySym = Symbol("key1")
+
+
+const jsUser = {
+    name: "Anshul",
+    age: 21,
+    location: "shimla",
+    isLoggedin: false, 
+    email: "s.anshul0311@gmial.com",
+
+    [mySym]: "mykey1"
+
+}
+
+console.log(jsUser.email) // wrong way to access
+
+console.log(jsUser["email"]) // right way
+
+console.log(jsUser[mySym])
+console.log(typeof(jsUser[mySym]))
+
+jsUser.email = "s.anshul0311@chatgpt.com"
+// Object.freeze(jsUser)
+jsUser.email = "s.anshul0311@meta.com"
+
+console.log(jsUser)
+
+jsUser.greeting = function(){
+    console.log("hello js user")
+}
+
+console.log(jsUser.greeting())
+
+jsUser.greetingTwo = function(){
+    console.log(`hello js user, ${this.name}`)
+}
+
+console.log(jsUser.greetingTwo())

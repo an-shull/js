7140933f88b61e4974642a44e5ff03f30a3be295
an-shull <118686764+an-shull@users.noreply.github.com>
2025-10-02 10:32:46 +0000

    obj3

---
+++ basics_2/obj_destruct.js
@@ -0,0 +1,17 @@
+
+//de structuring => 
+// syntactical sugar for easy code and acess of obj elements 
+
+
+const course ={
+    coursename: "js",
+    price: "696",
+    instructor: "hitesh"
+}
+
+//console.log(course.instructor)
+
+const{instructor} = course
+
+console.log(instructor)
+
